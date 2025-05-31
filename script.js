7a8844619c4e9f4078f11bb9524eed185a226a55
Anandhu Presannan <anandhupresannan@gmail.com>
2025-05-31 21:52:02 +0530

    first comit test

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navigation
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
